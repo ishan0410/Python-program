e69a613286f58ae08a044db8e6af2295975ed484
Ishan0410 <80149658+ishan0410@users.noreply.github.com>
2021-05-23 22:22:49 +0530

    Add files via upload

---
+++ snakegunwater.py
@@ -0,0 +1,40 @@
+print("you have 10 chances")
+import random
+score=0
+computer=0
+chance=0
+while(chance<10):
+    n=int(input("enter your choice \n 0-snake \n 1-water \n 2-gun"))
+    chance=chance+1
+    lst=["snake","water","gun"]
+    choice=random.choice(lst)
+    if choice =="snake":
+        if n==0:
+            print("its a tie")
+            score=score+0
+        elif n==1:
+            print("you lost water snake drank you water")
+            computer=computer+1
+        else:
+            print("you won sanke got shot")
+            score=score+1
+    elif choice=="water":
+        if n==0:
+            print("you won")
+            score=score+1
+        elif n==1:
+            print("its a tie")
+        else:
+            print("you lost")
+            computer=computer+1
+    else:
+        if n==0:
+            print("you lost")
+            computer =computer +1
+        elif n==1:
+            print("you won")
+            score=score+1
+        else:
+            print("its a tie")
+else:
+    print("game over",computer,score)

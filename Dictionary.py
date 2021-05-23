e49cd149ad7fe7278958282b3d52a8d129048a94
Ishan0410 <80149658+ishan0410@users.noreply.github.com>
2021-05-23 22:20:05 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,4 @@
+mywords = {"Basketball":"48 minutes game","Football":"90 minutes game","Cricket":"4 hours game","Pubg":"Unlimited time"}
+print("Enter the Sport to know its time of play ")
+word = input()
+print(mywords[word])

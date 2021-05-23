4917ccfc085a71d8f913f9fb6010f9b72f195cb1
Ishan0410 <80149658+ishan0410@users.noreply.github.com>
2021-05-23 22:13:55 +0530

    Add files via upload

---
+++ Adding.py
@@ -0,0 +1,12 @@
+# print ("Enter your two numbers to add: ")
+# a=input()
+# b=input()
+# print("Sum = ",int(a)+int(b))
+T = int(input())
+while T!=0:
+    a=int(input())
+    b=int(input())
+    print(a," + ",b," = ",end=" ")
+    ans=a+b
+    print(ans)
+    T=T-1
